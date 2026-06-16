23e5172da8d77af1a58b017f1d0c5c9322ac1404
ALEX-L1ANG <343293684@tdsb.ca>
2026-06-16 23:49:53 +0000

    update1

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,31 @@
 public class PracticeProblem {
 
 	public static void main(String args[]) {
-
+		
 	}
+
+	public static boolean hasCapital(String str) {
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            
+            if (Character.isUpperCase(c)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+	public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        
+        for (int i = 2; i < num; i++) {
+            if (num % i ==0) {
+                return false;
+            }
+        }
+        return true;
+    }
 
 }
